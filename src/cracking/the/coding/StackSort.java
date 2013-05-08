197d12037aecb6ee413f9da16556aa82ef9f43ad
Jun Yamog <jkyamog@gmail.com>
2013-05-08 21:41:57 +1200

    add stack sort, still has bugs

---
+++ src/cracking/the/coding/StackSort.java
@@ -0,0 +1,40 @@
+package cracking.the.coding;
+
+import java.util.Stack;
+
+public class StackSort {
+
+	public static void main(String[] args) {
+		
+		Stack<Integer> a = new Stack<Integer>();
+		Stack<Integer> b = new Stack<Integer>();
+		
+//		a.push(3);
+//		a.push(2);
+//		a.push(4);
+//		a.push(1);
+		a.push(4);
+		a.push(3);
+		a.push(2);
+		a.push(1);
+
+		while(!a.empty()) {
+			if (b.empty() || a.peek().compareTo(b.peek()) == -1) {
+				System.out.println("first");
+				b.push(a.pop());
+			} else {
+				Integer t = a.pop();
+				System.out.println("second " + t);
+				while (t.compareTo(b.peek()) == -1 && !b.empty()) {
+					a.push(b.pop());
+				}
+				b.push(t);
+			}
+		}
+		
+		while(!b.empty()) {
+			System.out.println(b.pop());
+		}
+	}
+
+}
