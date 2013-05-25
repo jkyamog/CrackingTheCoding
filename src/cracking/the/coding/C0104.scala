3c8e4cdbb38708c8acf9dd4011bb7652d6f6c2de
Jun Yamog <jkyamog@gmail.com>
2013-05-25 15:25:43 +1200

    fix isAnagram implementation, it was just is reversed

--- src/cracking/the/coding/C0104.scala
+++ src/cracking/the/coding/C0104.scala
@@ -7,32 +7,37 @@ object C0104 extends App {
 		else if (str1 == null || str2 == null) false
 		else if (str1.size != str2.size) false
 		else {
-			var isAnagram = true
-		
-			for (i <- 0 until str1.size) {
-				if (str1(i) != str2(str2.size - i - 1)) isAnagram = false
+			val b2 = str2.toBuffer
+			var i = 0
+			while (i < str1.size) {
+				var j = 0
+				while (j < b2.size) {
+					if (str1(i) == b2(j)) b2.remove(j)
+					j += 1
+				}
+				i += 1
 			}
 		
-			isAnagram
+			if (b2.size == 0) true else false
 		}
 	}
 	
 	def isAnagram2(str1: String, str2: String) = {
-		def isAnaInternal(head1: Char, tail1: String, head2: Char, tail2: String): Boolean = {
-			if (head1 == head2 && tail1.tail.isEmpty && tail2.tail.isEmpty) true
-			else if (head1 == head2) true && isAnaInternal(tail1.head, tail1.tail, tail2.head, tail2.tail)
+		def isAnaInternal(head1: Char, tail1: String, str2: String): Boolean = {
+			if (tail1.isEmpty && str2.size == 1 && str2.contains(head1.toString)) true
+			else if (tail1.isEmpty) false
+			else if (str2.contains(head1.toString)) isAnaInternal(tail1.head, tail1.tail, str2.replaceFirst(head1.toString, ""))
 			else false
 		}
 
 		(str1, str2) match {
 			case (null, null) => true
 			case (_, null) | (null, _) => false
-			case (str1, str2) =>
-				val reversed = str2.reverse
-				isAnaInternal(str1.head, str1, reversed.head, reversed)
+			case (str1, str2) => isAnaInternal(str1.head, str1.tail, str2)
 		}
 	}
 	
+	println(isAnagram("foo", "ofo"))
 	println(isAnagram("foo", "oof"))
 	println(isAnagram("fo", "off"))
 	println(isAnagram("xoo", "oof"))
@@ -41,7 +46,8 @@ object C0104 extends App {
 	println(isAnagram("o", null))
 	println(isAnagram("o", "o"))
 	println(isAnagram("f", "o"))
-	
+	println("----------------")
+	println(isAnagram2("foo", "ofo"))
 	println(isAnagram2("foo", "oof"))
 	println(isAnagram2("fo", "off"))
 	println(isAnagram2("xoo", "oof"))
