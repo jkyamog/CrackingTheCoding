ef67076a0e6e95e27a4bb470e4a87b21893670ae
Jun Yamog <jkyamog@gmail.com>
2013-05-31 21:00:39 +1200

    removing problem statement

--- src/cracking/the/coding/BadNeighbors.scala
+++ src/cracking/the/coding/BadNeighbors.scala
@@ -6,29 +6,6 @@ import scala.collection.mutable.Buffer
 
 http://community.topcoder.com/stat?c=problem_statement&pm=2402&rd=5009
 
-Problem Statement for BadNeighbors
-
-
-Problem Statement
-    	
-The old song declares "Go ahead and hate your neighbor", and the residents of Onetinville have taken those words to heart. Every resident hates his next-door neighbors on both sides. Nobody is willing to live farther away from the town's well than his neighbors, so the town has been arranged in a big circle around the well. Unfortunately, the town's well is in disrepair and needs to be restored. You have been hired to collect donations for the Save Our Well fund.
-
-Each of the town's residents is willing to donate a certain amount, as specified in the int[] donations, which is listed in clockwise order around the well. However, nobody is willing to contribute to a fund to which his neighbor has also contributed. Next-door neighbors are always listed consecutively in donations, except that the first and last entries in donations are also for next-door neighbors. You must calculate and return the maximum amount of donations that can be collected.
-
- 
-Definition
-    	
-Class:	BadNeighbors
-Method:	maxDonations
-Parameters:	int[]
-Returns:	int
-Method signature:	int maxDonations(int[] donations)
-(be sure your method is public)
-    
- 
-Constraints
--	donations contains between 2 and 40 elements, inclusive.
--	Each element in donations is between 1 and 1000, inclusive.
  */
 object BadNeighbors extends App {
 	
--- src/cracking/the/coding/TagalogDictionary.scala
+++ src/cracking/the/coding/TagalogDictionary.scala
@@ -3,37 +3,6 @@ package cracking.the.coding
 /**
 
 http://community.topcoder.com/stat?c=problem_statement&pm=7411
-
-Problem Statement
-    	
-In the first half of the 20th century, around the time that Tagalog became the national language of the Philippines, a standardized alphabet was introduced to be used in Tagalog school books (since then, the national language has changed to a hybrid "Pilipino" language, as Tagalog is only one of several major languages spoken in the Philippines).
-
-Tagalog's 20-letter alphabet is as follows:
-
-a b k d e g h i l m n ng o p r s t u w y
-Note that not all letters used in English are present, 'k' is the third letter, and 'ng' is a single letter that comes between 'n' and 'o'.
-
-You are compiling a Tagalog dictionary, and for people to be able to find words in it, the words need to be sorted in alphabetical order with reference to the Tagalog alphabet. Given a list of Tagalog words as a String[], return the same list in Tagalog alphabetical order.
-
- 
-Definition
-    	
-Class:	TagalogDictionary
-Method:	sortWords
-Parameters:	String[]
-Returns:	String[]
-Method signature:	String[] sortWords(String[] words)
-(be sure your method is public)
-    
- 
-Notes
--	Any 'n' followed followed by a 'g' should be considered a single 'ng' letter (the one that comes between 'n' and 'o').
- 
-Constraints
--	words will contain between 1 and 50 elements, inclusive.
--	Each element of words will contain between 1 and 50 characters, inclusive.
--	Each character of each element of words will be a valid lowercase letter that appears in the Tagalog alphabet.
--	Each element of words will be distinct.
  
 */
 
