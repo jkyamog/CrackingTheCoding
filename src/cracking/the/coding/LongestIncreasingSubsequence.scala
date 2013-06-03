0658b573068931db03897ab4b94561466eaa50e2
Jun Yamog <jkyamog@gmail.com>
2013-06-03 15:45:19 +1200

    trying with streams

--- src/cracking/the/coding/LongestIncreasingSubsequence.scala
+++ src/cracking/the/coding/LongestIncreasingSubsequence.scala
@@ -19,11 +19,52 @@ object LongestIncreasingSubsequence extends App {
 		}
 	}
 	
-	val snums = nums.toStream
+	def LISS(head: Int, list: Stream[Int]): Stream[Stream[Int]] = {
+		if (list.isEmpty) Stream(head #:: Stream.empty)
+		else {
+			for {
+				l <- Stream(head) #:: LISS(list.head, list.tail)
+			} yield {
+				if (!l.isEmpty && (head < l.head))
+					head #:: l
+				else
+					l
+			}
+		}
+	}
+	
+	def LISS2(head: Int, list: Stream[Int]): Stream[Int] = {
+		if (list.isEmpty) head #:: Stream.empty
+		else {
+			head #:: LISS2(list.head, (list.tail filter (_> head)))
+		}
+	}
+	
+	def LISS3(list: Stream[Int]): Stream[Stream[Int]] = {
+		if (list.isEmpty) Stream.empty
+		else
+			for {
+				l <- list
+			} yield {
+				LISS2(l, list.tail) 
+			}
+	}
+	
 	val lis1 = LIS(1, List(2))
 	val lis2 = LIS(2, List(1))
 	val lis3 = LIS(nums.head, nums.tail)
 	
-	lis3 sortBy (_.length) foreach println
+//	lis3 sortBy (_.length) foreach println
+
+//	val snums = nums.toStream
+	val snums = List(2, 6, 4, 5, 1, 3).toStream
+//	val snums = List(2, 1).toStream
+	val lis4 = LISS2(snums.head, snums.tail)
+	
+	lis4.take(snums.length) foreach { l =>
+//		println(l.take(7).toList)
+		println(l)
+	}
+
 	
 }
