c7cae2edb79c61976442e08e768c2afa4a1e1ce7
Jun Yamog <jkyamog@gmail.com>
2015-02-26 14:11:07 +1300

    tried LIS again, finally works but still in pretty much exponential cost

--- src/cracking/the/coding/LongestIncreasingSubsequence.scala
+++ src/cracking/the/coding/LongestIncreasingSubsequence.scala
@@ -4,79 +4,29 @@ object LongestIncreasingSubsequence extends App {
 	
 	val nums = List(0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15)
 
-	def LIS(head: Int, list: List[Int]): List[List[Int]] = {
-		if (list.isEmpty) List(head :: Nil)
-		else {
-			for {
-				n <- (0 until list.length).toList
-				sub <- LIS(list(n), list.takeRight(list.length - 1 - n))
-			} yield {
-				if (head < sub.head)
-					head :: sub
-				else
-					head :: Nil
-			}
-		}
-	}
-	
-	def LISS(head: Int, list: Stream[Int]): Stream[Stream[Int]] = {
-		if (list.isEmpty) Stream(head #:: Stream.empty)
-		else {
-			for {
-				l <- Stream(head) #:: LISS(list.head, list.tail)
-			} yield {
-				if (!l.isEmpty && (head < l.head))
-					head #:: l
-				else
-					l
-			}
-		}
-	}
-	
-	def LISS2(head: Int, list: Stream[Int]): Stream[Int] = {
-		if (list.isEmpty) head #:: Stream.empty
-		else {
-			head #:: LISS2(list.head, (list.tail filter (_> head)))
-		}
-	}
-	
-	def LISS3(list: List[Int]): List[Stream[Int]] = {
-		
-		val paths = for {
-			i <- 0 until list.length
-			j <- (i + 1) until list.length
-		} yield {
-			val current = list(i)
-			val next = list(j)
-			println(s"current $current next $next")
-			if (current < next)
-				current #:: Stream(next)
-			else
-				current #:: Stream.empty
-		}
-		
-		paths foreach println
-		
-		paths.toList
-		
-	}
-	
-	
-	val lis1 = LIS(1, List(2))
-	val lis2 = LIS(2, List(1))
-	val lis3 = LIS(nums.head, nums.tail)
-	
-//	lis3 sortBy (_.length) foreach println
+  def bruteForceLIS(nums: Seq[Int]) = {
+    def allCombo(currentList: Seq[Int], toPick: Seq[Int]): Seq[Seq[Int]] = {
+      if (toPick.isEmpty) {
+        Seq(currentList)
+      } else if (toPick.size == 1) {
+        Seq(currentList :+ toPick.head)
+      } else {
+        for {
+          n <- toPick
 
-//	val snums = nums.toStream
-	val tnums = List(2, 6, 4, 5, 1, 3)
-//	val tnums = List(1, 2)
-	val lis4 = LISS3(tnums)
-	
-	lis4.take(tnums.length) foreach { l =>
-		println(l.take(7).toList)
-//		println(l)
-	}
+          rest = toPick.dropWhile(_ != n) // get everything to the right of n
+                      .tail               // remove n
+                      .filter(_ > n)      // only get elements bigger than n, useless to compute the other combo, looking for increasing values only
+          c <- allCombo(currentList :+ n, rest) // now add the n to the list chosen, use the rest for further computation
+        } yield c
+      }
+    }
 
-	
+    allCombo(Seq.empty, nums).sortBy(_.size).last // get the biggest sized result
+  }
+
+
+  println(bruteForceLIS(nums))
+
+// List(0, 2, 6, 9, 11, 15)
 }
